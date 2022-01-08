3897260460b516b6210cc4c27687100f60e5c763
bluemmroom <bluemmroom@mail.ru>
2022-01-08 10:02:52 +0500

    Writing Binary Files with Java NIO

--- src/com/bluemmroom/Main.java
+++ src/com/bluemmroom/Main.java
@@ -1,8 +1,38 @@
 package com.bluemmroom;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        try (FileOutputStream binFIle = new FileOutputStream("data.dat");
+             FileChannel binChannel = binFIle.getChannel()) {
+
+            byte[] outputBytes = "Hello World!".getBytes();
+            ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
+            int numBytes = binChannel.write(buffer);
+            System.out.println("numBytes written was: " + numBytes);
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(245);
+            intBuffer.flip();
+            numBytes = binChannel.write(intBuffer);
+            System.out.println("numBytes written was: " + numBytes);
+
+            intBuffer.flip();
+            intBuffer.putInt(-98765);
+            intBuffer.flip();
+            numBytes = binChannel.write(intBuffer);
+            System.out.println("numBytes written was: " + numBytes);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         /**     Reading and Writing Java NIO
          * try {
