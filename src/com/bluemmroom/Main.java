7d93bba6bebb89036fe586698b25d541e3ad03b4
bluemmroom <bluemmroom@mail.ru>
2022-01-08 09:32:20 +0500

    First NIO

---
+++ src/com/bluemmroom/Main.java
@@ -0,0 +1,21 @@
+package com.bluemmroom;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /**     Reading and Writing Java NIO
+         * try {
+         *             Path dataPath = FileSystems.getDefault().getPath("data.txt");
+         *             Files.write(dataPath, "\nLine 5".getBytes("UTF-8"), StandardOpenOption.APPEND);
+         *             List<String> lines = Files.readAllLines(dataPath);
+         *             for (String line : lines) {
+         *                 System.out.println(line);
+         *             }
+         *         } catch (IOException e) {
+         *             e.printStackTrace();
+         *         }
+         */
+
+    }
+}
